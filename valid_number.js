082d5def4212aa482d19b9740fe3fef6d9aaa26b
gislane.nunes <gislane_nunes@yahoo.com.br>
2023-01-23 17:24:15 -0300

    desafios leetcode enunciados

---
+++ valid_number.js
@@ -0,0 +1,15 @@
+// A valid number can be split up into these components (in order):
+
+// A decimal number or an integer.
+// (Optional) An 'e' or 'E', followed by an integer.
+// A decimal number can be split up into these components (in order):
+
+// (Optional) A sign character (either '+' or '-').
+// One of the following formats:
+// One or more digits, followed by a dot '.'.
+// One or more digits, followed by a dot '.', followed by one or more digits.
+// A dot '.', followed by one or more digits.
+// An integer can be split up into these components (in order):
+
+// (Optional) A sign character (either '+' or '-').
+// One or more digits.
